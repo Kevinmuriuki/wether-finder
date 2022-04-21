10a87f928a04445e3d4abda2fcfd6bc4fb57c1c0
Kevinmuriuki <kevinmuriukiwaweru@gmail.com>
2022-04-21 21:47:44 +0300

    Add API call

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,12 +1,25 @@
-const title = { title: 'Weather Finder' };
+const axios = require("axios");
+const { response } = require("../app");
+const API_KEY = "341f3a5ad73fcf20c2dee19a9f0b6b90";
+
 
 exports.homePage = (req, res, next) => {
-    res.render('index', title)
+    res.render('index')
 
 }
 
 exports.getWeather = (req, res, next) => {
-    res.send(`You typed ${req.body.city}`);
+    const city = req.body.city;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    
+    axios.get(url)
+        .then(response => {
+            res.render('index', { weather: `it is currently ${response.data.main.temp} in ${response.data.name}` })
+            
+        })
+        .catch(err => {
+            console.log(err);
+        })
 }
 
 
